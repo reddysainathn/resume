b5e18e147d1f12115f48b5006bf5a8375afa4248
Sainath <sainathreddynarahari6@gmail.com>
2020-10-04 21:35:23 -0400

    Updates

---
+++ precache-manifest.7128e4ec45afd3faf8354bc3a3a2acb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4a32aa525f5e8b1f7f039ef919c3ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1b7a0f3f30af4c7f3cd",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "98a68b67b921b5b7b0ca",
+    "url": "/static/js/2.11cf55e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.11cf55e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b7a0f3f30af4c7f3cd",
+    "url": "/static/js/main.283d9acf.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
